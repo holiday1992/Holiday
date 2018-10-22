bac08c053d93a820715424485d50311f597d689f
Zehong <33176715+holiday1992@users.noreply.github.com>
2018-10-22 02:45:28 -0700

    Remove Duplicate From Array

---
+++ RemoveDuplicate.cs
@@ -0,0 +1,28 @@
+﻿namespace Array
+{
+    public class RemoveDuplicate
+    {
+        public int RemoveDuplicates(int[] nums)
+
+        {
+            int i = 0;
+            nums[i++] = nums[0];
+            if (i < nums.Length)
+
+            {
+                for (int j = 1; j < nums.Length; j++)
+                {
+                    if (nums[j] != nums[j - 1])
+                    {
+                        nums[i++] = nums[j];
+                    }
+
+                }
+
+            }
+
+            return i;
+
+        }
+    }
+}
